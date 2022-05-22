834ec876b496b1ebbbddf94a4da96f864a6e769b
qufeng107 <qufeng107@gmail.com>
2022-05-22 22:09:13 +0100

    steup tool

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Detection of Manipulated Pricing in Smart Energy CPS Scheduling",
+    version="1.0",
+    author="Feng Qu",
+    author_email="qufeng107@qq.com",
+    description="Detection of Manipulated Pricing. Calculate scheduling results",
+
+    url="https://github.com/qufeng107/COMP3217Coursework.git", 
+
+    packages=find_packages()
+)
